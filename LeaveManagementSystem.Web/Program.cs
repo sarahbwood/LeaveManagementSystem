356ce9f07d5c46d2d8ea3812aaf52d0683940e86
Sarah <Sarahbwood15@gmail.com>
2025-06-13 10:53:49 +0200

    Add AspNET Identity

--- LeaveManagementSystem.Web/Program.cs
+++ LeaveManagementSystem.Web/Program.cs
@@ -1,4 +1,24 @@
+using LeaveManagementSystem.Web.Areas.Identity.Data;
+using LeaveManagementSystem.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder
+    .Configuration
+    .GetConnectionString("LeaveManagementSystemWebContextConnection") ?? throw new InvalidOperationException("Connection string 'LeaveManagementSystemWebContextConnection' not found.");
+
+builder.Services.AddDbContext<LeaveManagementSystemWebContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<LeaveManagementSystemWebUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LeaveManagementSystemWebContext>();
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
