a40518c5f91b0470dd6d946f6c87a4c570ebbb53
Sarah <Sarahbwood15@gmail.com>
2025-09-01 09:19:30 +0200

    Add basic leave calendar UI

---
+++ LeaveManagementSystem.Web/Controllers/LeaveCalendarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class LeaveCalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
